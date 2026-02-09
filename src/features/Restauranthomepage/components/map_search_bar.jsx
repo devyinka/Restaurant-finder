be6b04e4558cc090f4c7a0f83214fa2b1c0d390c
devyinka <217584194+devyinka@users.noreply.github.com>
2026-02-09 02:55:13 +0100

    modified:   src/features/Restauranthomepage/components/map_search_bar.jsx 	modified:   src/features/Restauranthomepage/components/searchbar.jsx

--- src/features/Restauranthomepage/components/map_search_bar.jsx
+++ src/features/Restauranthomepage/components/map_search_bar.jsx
@@ -8,7 +8,7 @@ const MapSearchBar = () => {
   const [search, setSearch] = useState(keyword);
   useEffect(() => {
     Search(search);
-  }, [search]);
+  }, [Search, keyword]);
 
   return (
     <View style={styles.search}>
